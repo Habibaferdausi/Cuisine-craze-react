016e8739540aef3e8e1b95858ef55b45ac18131c
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-06 14:08:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-300 underline">
+        Hello world not me!
+      </h1>
+    </>
+  );
+}
+
+export default App;
